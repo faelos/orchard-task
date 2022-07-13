1b6a3718ac2def37482bdcc521b73e4c4f4924e5
Greg McDonald <greg.mcdonald.dev@gmail.com>
2022-07-14 08:33:18 +1000

    feat: modal registration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import VueEasyLightbox from 'vue-easy-lightbox'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueEasyLightbox)
+app.mount('#app')
