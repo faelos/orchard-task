e50722215fe4525e992751176c02f8982685cac5
Greg McDonald <greg.mcdonald.dev@gmail.com>
2022-07-14 07:59:57 +1000

    chore: content typos

--- src/lib/content-provider.js
+++ src/lib/content-provider.js
@@ -1,6 +1,6 @@
 const hero = {
   heading: 'Answer your body’s needs',
-  desc: 'The way ingredients are sources affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body’s needs.',
+  desc: 'The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body’s needs.',
   subheading: 'Be mindful',
   subcontent: 'Sourcing local or organic food is a good way to start being more mindful about what you’re cooking and eating.',
   images: [
@@ -40,7 +40,7 @@ const latestArticles = {
     },
     {
       title: 'Taking Taste Further',
-      desc: 'This exclusive cookbook gives you all the know-how you need. We’ve designed it to make sure you get the most out of our products — and the best our of your food.',
+      desc: 'This exclusive cookbook gives you all the know-how you need. We’ve designed it to make sure you get the most out of our products — and the best out of your food.',
       cta: 'Read more',
       imgSrc: 'latest-article-3@2x.jpg',
       imgAlt: 'img desc 3',
